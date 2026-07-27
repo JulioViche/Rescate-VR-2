bf1944c10404739ca91b5f5275e084f6493ccd1f
JulioViche <jvichecastillo@gmail.com>
2026-07-26 23:39:18 -0500

    feat: add main menu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
